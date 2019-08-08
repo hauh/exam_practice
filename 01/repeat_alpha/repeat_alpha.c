1e3ce8e7c168cbf334593f982bc33f5636b098c4
smorty <hauh@ya.ru>
2019-08-08 23:01:36 +0300

    00 01

---
+++ 01/repeat_alpha/repeat_alpha.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/08 20:49:38 by smorty            #+#    #+#             */
+/*   Updated: 2019/08/08 22:58:05 by smorty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int count;
+
+	if (argc == 2)
+	{
+		++argv;
+		while (**argv)
+		{
+			count = 1;
+			if (**argv > 'A' && **argv <= 'Z')
+				count += **argv - 'A';
+			else if (**argv > 'a' && **argv <= 'z')
+				count += **argv - 'a';
+			while (count--)
+				write(1, *argv, 1);
+			++(*argv);
+		}
+	}
+	return (write(1, "\n", 1));
+}
---
+++ 01/search_and_replace/search_and_replace.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/08 21:12:00 by smorty            #+#    #+#             */
+/*   Updated: 2019/08/08 21:37:30 by smorty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+static int	valid_args(char *arg1, char *arg2)
+{
+	if ((*arg1 < 'A' || (*arg1 > 'Z' && *arg1 < 'a') || *arg1 > 'z')
+		|| (*arg2 < 'A' || (*arg2 > 'Z' && *arg2 < 'a') || *arg2 > 'z')
+		|| (*(arg1 + 1) || *(arg2 + 1)))
+		return (0);
+	return (1);
+}
+
+int			main(int argc, char **argv)
+{
+	char *p;
+
+	if (argc == 4 && valid_args(*(argv + 2), *(argv + 3)))
+	{
+		p = *(++argv);
+		while (*p)
+		{
+			if (*p == **(argv + 1))
+				*p = **(argv + 2);
+			++p;
+		}
+		write(1, *argv, p - *argv);
+	}
+	return (write(1, "\n", 1));
+}
